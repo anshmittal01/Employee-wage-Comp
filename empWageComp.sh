ad734e619e22222b10b41a465243ecb5bcab1c22
Ansh Mittal <mittalansh22@gmail.com>
2020-07-14 11:36:46 +0530

    Employee attendance check added

--- empWageComp.sh
+++ empWageComp.sh
@@ -1,2 +1,12 @@
 #! /bin/bash -x
-echo "welcome to Welcome to Employee Wage Computation Program"
+echo "Welcome to Employee Wage Computation Program"
+
+#check employee is present or absent
+
+attendance_check=$(( RANDOM%2 ))
+if [ $attendance_check -eq 1 ]
+then
+	echo "employee is present"
+else
+	echo "employee is absent"
+fi
