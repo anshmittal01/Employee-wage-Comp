fbadd5150880628018d9c39ed554eba9ec783f48
Ansh Mittal <mittalansh22@gmail.com>
2020-07-14 13:12:47 +0530

    functional implementation added

--- empWageComp.sh
+++ empWageComp.sh
@@ -8,24 +8,27 @@ workingDays=20
 workingHrs=100
 totalWorkHrs=0
 totalWorkDays=0
-while [[ $totalWorkHrs -lt $workingHrs && $totalWorkDays -lt $workingDays ]]
-do
-	attendance_check=$(( RANDOM%3 ))
-	case $attendance_check in
+
+getWorkHours()
+{
+	case $1 in
 		0)
-			echo "employee is full time present"
 			empHrs=8
 			(( totalWorkDays++ ));;
 		1)
-			echo "employee is half time present"
 			empHrs=4
 			(( totalWorkDays++ ));;
 		2)
-			echo "employee is absent"
 			empHrs=0;;
 		*)
-			echo "INVALID";;
+			;;
 	esac
+}
+
+while [[ $totalWorkHrs -lt $workingHrs && $totalWorkDays -lt $workingDays ]]
+do
+	attendance_check=$(( RANDOM%3 ))
+	getWorkHours $attendance_check
 	totalWorkHrs=$(( $totalWorkHrs+$empHrs ))
 	salary=$(( $empRatePerHr*$empHrs ))
 	totalSalary=$(( $totalSalary+$salary ))
